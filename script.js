c809dbc5e14f976ee0f2ab7b56f3b90dbf57e5ba
Clov23 <gerardodev408@outlook.com>
2026-03-23 18:23:17 -0600

    fix: css

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-//Animacion al hacer scroll
-const hiddElements = document.querySelectorAll(".hidden");
+const hiddenElements = document.querySelectorAll(".hidden");
 
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach(entry =>{
-        if(entry.isIntersecting){
-            entry.target.classList.add("show");
-        }
-    });
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
 });
 
-hiddElements.forEach(elem => observer.observe(elem));
+hiddenElements.forEach((el) => observer.observe(el));
 
-//boton interactivo
-document.getElementById("about-btn").addEventListener("click", ()=>{
-    alert("Somos una tienda enfocada en calidad, innovación y rendimiento deportivo");
-})
+document.getElementById("about-btn")?.addEventListener("click", () => {
+  alert(
+    "Somos una tienda enfocada en calidad, innovación y rendimiento deportivo."
+  );
+});
